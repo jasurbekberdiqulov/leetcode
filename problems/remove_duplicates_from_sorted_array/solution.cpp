8e70d1173c06168a002eea6ed238b6164e04367a
Jasurbek Berdiqulov <56794453+jasurbekberdiqulov@users.noreply.github.com>
2023-01-24 19:47:57 +0000

    Sync LeetCode submission - Remove Duplicates from Sorted Array (cpp)

---
+++ problems/remove_duplicates_from_sorted_array/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        for(int i = 0; i < nums.size() - 1; ++i) {
+            if(nums[i] == nums[i + 1]) {
+                nums.erase(nums.begin() + i);
+                --i;
+            }
+        }
+        return nums.size();        
+    }
+};
