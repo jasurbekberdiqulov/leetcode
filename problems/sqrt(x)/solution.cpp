f2dc379b5e74b179525a4f3b07af34f59e0b1fcc
Jasurbek Berdiqulov <56794453+jasurbekberdiqulov@users.noreply.github.com>
2023-01-25 19:13:04 +0000

    Sync LeetCode submission - Sqrt(x) (cpp)

--- problems/sqrt(x)/solution.cpp
+++ problems/sqrt(x)/solution.cpp
@@ -1,16 +1,15 @@
 class Solution {
+#define ll long long
 public:
-    int check(int x, int s) {
-        if(x == 0) return 0;
-        long long m = x / 2 + x % 2;
-        return (m * m <= s)? m : check(m, s);
-    }
     int mySqrt(int x) {
-        int a = check(x, x);
-        for(long long i = a; i < 46342; ++i) {
-            if(i * i > x) return i - 1;
+        int left = 0, right = x;
+        while(left < right){
+            ll middle = left + (((ll)right - left + 1) >> 1);
+            if(middle * middle <= (ll)x)
+                left = middle;
+            else
+                right = middle - 1;
         }
-        //But it will never come to this :))
-        return 0;
+        return left;
     }
 };
