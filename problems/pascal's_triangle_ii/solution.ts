366ee445d937b88b45eb8c1d6a423fd61d9e378b
Jasurbek Berdiqulov <56794453+jasurbekberdiqulov@users.noreply.github.com>
2023-01-27 21:45:22 +0000

    Sync LeetCode submission - Pascal's Triangle II (typescript)

--- problems/pascal's_triangle_ii/solution.ts
+++ problems/pascal's_triangle_ii/solution.ts
@@ -1,13 +1,15 @@
 function getRow(rowIndex: number): number[] {
-    let array:number[][] = [[1]];
+    let prevArray:number[] = [];
 
-    for(let i = 1; i < rowIndex + 1; ++i) {
-        array[i] = [];
-        array[i][0] = 1;
+    for(let i = 0; i < rowIndex + 1; ++i) {
+        const array = [];
+        array[0] = 1;
         for(let j = 1; j < i; ++j) {
-            array[i][j] = array[i - 1][j - 1] + array[i - 1][j]
+            array[j] = prevArray[j - 1] + prevArray[j]
         }
-        array[i][i] = 1;
+        array[i] = 1;
+        prevArray = array;
+        console.log(prevArray);
     }
-    return array[rowIndex];
+    return prevArray;
 };
