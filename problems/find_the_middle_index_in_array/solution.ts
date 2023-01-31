8a5c111363213d8bcbe2565da0df266d43bed955
Jasurbek Berdiqulov <56794453+jasurbekberdiqulov@users.noreply.github.com>
2023-01-31 07:03:47 +0000

    Sync LeetCode submission - Find the Middle Index in Array (typescript)

---
+++ problems/find_the_middle_index_in_array/solution.ts
@@ -0,0 +1,17 @@
+function findMiddleIndex(nums: number[]): number {
+    let sumL = 0, sumAll = 0;
+    if(nums.length == 1) return 0;
+    if(nums.length == 2 && nums[1] == 0) return 0;
+
+    for(let i = 0; i < nums.length; ++i) {
+        sumAll += nums[i];
+    }
+
+    if(sumAll - nums[0] == 0) return 0;
+
+    for(let i = 1; i < nums.length; ++i) {
+        sumL += nums[i-1];
+        if(2*sumL == sumAll - nums[i]) return i;
+    }
+    return -1;
+};
