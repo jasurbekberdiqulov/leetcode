de6b9742ad79f14db2f0ebf513323b8920373d06
Jasurbek Berdiqulov <56794453+jasurbekberdiqulov@users.noreply.github.com>
2023-02-01 19:42:15 +0000

    Sync LeetCode submission - Reverse Linked List (typescript)

--- problems/reverse_linked_list/solution.ts
+++ problems/reverse_linked_list/solution.ts
@@ -1,29 +1,12 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
- */
-
 function reverseList(head: ListNode | null): ListNode | null {
-    let node : ListNode;
-    let array = []
-    while(head) {
-        array.push(head.val);
+    if(head == null) return null;
+    let temp = new ListNode(head.val);
+    head = head.next;
+    while (head) {
+        let _node = new ListNode(head.val);
+        _node.next = temp;
+        temp = _node;
         head = head.next;
     }
-    let len = array.length;
-    if(len) node = new ListNode(array[0], null); else return null;
-    let lastNode = node
-
-    for(let i = len - 1; i > -1; --i) {
-        lastNode.next = new ListNode(array[i], null);
-        lastNode = lastNode.next;
-    }
-    return node.next;
+    return temp;
 };
