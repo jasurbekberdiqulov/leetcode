5e132e954bc05aa4206c9229940f692bb3ba9706
Jasurbek Berdiqulov <56794453+jasurbekberdiqulov@users.noreply.github.com>
2023-02-01 08:09:16 +0000

    Sync LeetCode submission - Reverse Linked List (typescript)

--- problems/reverse_linked_list/solution.ts
+++ problems/reverse_linked_list/solution.ts
@@ -11,59 +11,6 @@
  */
 
 function reverseList(head: ListNode | null): ListNode | null {
-    interface IStack<T> {
-        push(item: T): void;
-        pop(): T | undefined;
-        peek(): T | undefined;
-        size(): number;
-    }
-    class Stack<T> implements IStack<T> {
-        private storage: T[] = [];
-
-        constructor(private capacity: number = Infinity) {}
-
-        push(item: T): void {
-            if (this.size() === this.capacity) {
-                // this is just from common error issues list.
-                throw Error("Stack has reached max capacity, you cannot add more items");
-            }
-            this.storage.push(item);
-        }
-
-        pop(): T | undefined {
-            return this.storage.pop();
-        }
-
-        peek(): T | undefined {
-            return this.storage[this.size() - 1];
-        }
-
-        size(): number {
-            return this.storage.length;
-        }
-    }
-
-    let node : ListNode, workerNode: ListNode | null;
-    const stack = new Stack<ListNode>();
-
-    while(head) {
-        workerNode = head;
-        head = head.next;
-        workerNode.next = null;
-        stack.push(workerNode);
-    }
-    let len = stack.size();
-    if(len) node = stack.pop(); else return null;
-    let lastNode = node
-
-    while(stack.size()) {
-        lastNode.next = stack.pop();
-        lastNode = lastNode.next;
-    }
-    return node;
-};
-
-function reverseList2(head: ListNode | null): ListNode | null {
     let node : ListNode;
     let array = []
     while(head) {
