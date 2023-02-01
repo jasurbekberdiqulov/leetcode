90191907e00c123833f22862f65fed8127956ad8
Jasurbek Berdiqulov <56794453+jasurbekberdiqulov@users.noreply.github.com>
2023-02-01 06:25:32 +0000

    Sync LeetCode submission - Contains Duplicate III (typescript)

--- problems/contains_duplicate_iii/solution.ts
+++ problems/contains_duplicate_iii/solution.ts
@@ -1,9 +1,14 @@
 function containsNearbyAlmostDuplicate(nums: number[], indexDiff: number, valueDiff: number): boolean {
-    for (let i = 0; i < nums.length; ++i) {
-        for(let j = i + 1; j < nums.length; ++j) {
-            if(j - i <= indexDiff) {
-                if(Math.abs(nums[i] - nums[j]) <= valueDiff) return true;
-            }
+    let i = 0;
+    let j = 0;
+
+    while (i < nums.length - 1) {
+        j += 1;
+        if (Math.abs(i - j) <= indexDiff && Math.abs(nums[i] - nums[j]) <= valueDiff) return true;
+
+        if (j - i === indexDiff) {
+            i += 1;
+            j = i;
         }
     }
     return false;
