8cbc79d08be2ca00abdfda29c6e25fa8b55e08ab
Jasurbek Berdiqulov <56794453+jasurbekberdiqulov@users.noreply.github.com>
2023-02-03 21:00:10 +0000

    Sync LeetCode submission - Swapping Nodes in a Linked List (typescript)

--- problems/swapping_nodes_in_a_linked_list/solution.ts
+++ problems/swapping_nodes_in_a_linked_list/solution.ts
@@ -1,73 +1,22 @@
 function swapNodes(head: ListNode | null, k: number): ListNode | null {
     if (head.next == null) return head;
 
-    let right: ListNode = head, prevRight: ListNode;
-    let node = right, counter = 0;
-    while (node) {
+    let right: ListNode;
+    let left = head, counter = 0;
+    while (left) {
         ++counter;
-        node = node.next;
-    }
-    if (k << 1 == counter + 1) return head;
-
-    if (k == 1 || k == counter) {
-        right = head;
-        prevRight = null;
-        while (right.next) {
-            prevRight = right;
-            right = right.next;
-        }
-        prevRight.next = null;
-
-        let temp = head;
-        head = head.next;
-        temp.next = null;
-
-        if (head) {
-            prevRight.next = temp;
-            right.next = head;
-        } else {
-            right.next = prevRight;
-        }
-
-        return right;
-    }
-
-    let l = counter - k;
-    if (k > l) {
-        const s = k - 1;
-        k = l + 1;
-        l = s;
-    }
-
-    for (let i = 1; i < k; ++i) {
-        prevRight = right;
-        right = right.next;
-    }
-
-    if (k << 1 == counter) {
-        prevRight.next = right.next;
-        const temp = right.next.next;
-        right.next = null;
-
-        prevRight.next.next = right;
-        prevRight.next.next.next = temp;
-        return (counter == 4) ? prevRight : head;
-    }
-
-    let left: ListNode = head, prevLeft: ListNode;
-    for (let i = 0; i < l; ++i) {
-        prevLeft = left;
+        if(counter == k) right = left;
         left = left.next;
     }
 
-    prevRight.next = left;
-    let temp = right.next;
-    right.next = left.next;
-    left.next = temp;
-    while(temp.next != left) {
-        temp = temp.next
-    }
-    temp.next = right;
+    if (k << 1 == counter + 1) return head;
 
-    return (counter == 5) ? prevRight : head;
+    left = head;
+    counter -= k - 1;
+    for (let i = 1; i < counter; ++i) {
+        left = left.next;
+    }
+
+    [right.val, left.val] = [left.val, right.val];
+    return head;
 };
